a346947b5c2d003ae8f39fc703b5308ca6e4dc44
Charlotte <70633528+CharlotteH86@users.noreply.github.com>
2020-11-24 15:30:48 +0100

    final changes

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 //let color = document.querySelector(".input").value;
 const clickBtn = document.querySelector("button");
 const boxColor = document.querySelector(".box");
+const headerEl = document.querySelector(".header");
+
 
 (function (){
-function changeColor(){
-    let color = document.querySelector(".input").value;
-    boxColor.style.backgroundColor = color;
-}
-
 
 clickBtn.addEventListener("click", (e) => {
-    changeColor();
-    e.preventDefault();
+e.preventDefault();
+  let color = document.querySelector(".input").value;
+
+  // Dispatch "ChangeColor" event.
+  changeColor(color);
+});
+
+function changeColor (c){
+  // Create "ChangeColor" event.
+  const event = new CustomEvent("colorChanger", { 
+    detail: {
+      color: c
+    }
   });
+  
+  // Dispatch event to box element and text element.
+  boxColor.dispatchEvent(event);
+  headerEl.dispatchEvent(event);
+}
+
+// Add event listener for box element.
+boxColor.addEventListener("colorChanger", (e)=>{
+  let color = e.detail.color;
+  boxColor.style.backgroundColor = color;
+})
+
+// Add event listener for text element.
+headerEl.addEventListener("colorChanger", (e)=>{
+  let colorText = e.detail.color;
+  headerEl.innerText = colorText;
+})
+
 })();
